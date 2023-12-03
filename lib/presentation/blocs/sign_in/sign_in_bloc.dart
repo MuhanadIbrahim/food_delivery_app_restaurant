9bc65af9c461178cbcedb21f61d6cc95507f05d3
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-12-04 00:18:28 +0200

    finish the main bloc of the sign in bloc

--- lib/presentation/blocs/sign_in/sign_in_bloc.dart
+++ lib/presentation/blocs/sign_in/sign_in_bloc.dart
@@ -1,13 +1,36 @@
-import 'package:bloc/bloc.dart';
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_delivery_app_restaurant/data/restaurant_repository.dart';
 
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(SignInInitial()) {
-    on<SignInEvent>((event, emit) {
-      // TODO: implement event handler
+  final RestaurantRepository _restaurantRepository;
+  SignInBloc({required RestaurantRepository restaurantRepository})
+      : _restaurantRepository = restaurantRepository,
+        super(SignInInitial()) {
+    on<SignInRequired>((event, emit) async {
+      emit(SignInProcess());
+
+      try {
+        var x = await _restaurantRepository.signIn(event.email, event.password);
+        if (x) {
+          emit(SignInSuccess());
+        } else {
+          emit(const SignInFailure());
+        }
+      } catch (e) {
+        log(e.toString());
+        emit(const SignInFailure());
+      }
     });
+    on<SignOutRequired>(
+      (event, emit) async {
+        await _restaurantRepository.logOut();
+      },
+    );
   }
 }
