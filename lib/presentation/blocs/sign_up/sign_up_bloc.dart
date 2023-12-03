0b17dd3840ec3b30806d6739b2cea43244af32df
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-12-04 00:28:04 +0200

    finish the main bloc class of the sign up bloc

--- lib/presentation/blocs/sign_up/sign_up_bloc.dart
+++ lib/presentation/blocs/sign_up/sign_up_bloc.dart
@@ -1,14 +1,29 @@
-import 'package:bloc/bloc.dart';
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_delivery_app_restaurant/data/restaurant_repository.dart';
 import 'package:food_delivery_app_restaurant/domain/restaurant.dart';
 
 part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(SignUpInitial()) {
-    on<SignUpEvent>((event, emit) {
-      // TODO: implement event handler
+  final RestaurantRepository _restaurantRepository;
+  SignUpBloc({required RestaurantRepository restaurantRepository})
+      : _restaurantRepository = restaurantRepository,
+        super(SignUpInitial()) {
+    on<SignUpRequired>((event, emit) async {
+      emit(SignUpProcess());
+      try {
+        MyRestaurant restaurant = await _restaurantRepository.signUp(
+            event.restaurant, event.password);
+        await _restaurantRepository.setRestaurantData(restaurant);
+        emit(SignUpSuccess());
+      } catch (e) {
+        log(e.toString());
+        emit(SignUpFailure());
+      }
     });
   }
 }
