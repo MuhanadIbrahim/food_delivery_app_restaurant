36cee5b2e8ff32627e5112edb7068b89b25fc14e
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-12-04 01:04:41 +0200

    finish the main bloc of the update_restaurant_info_bloc

--- lib/presentation/blocs/update_restaurant_info_bloc/update_restaurant_info_bloc_bloc.dart
+++ lib/presentation/blocs/update_restaurant_info_bloc/update_restaurant_info_bloc_bloc.dart
@@ -1,14 +1,25 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_delivery_app_restaurant/data/restaurant_repository.dart';
 
 part 'update_restaurant_info_bloc_event.dart';
 part 'update_restaurant_info_bloc_state.dart';
 
 class UpdateRestaurantInfoBloc
     extends Bloc<UpdateRestaurantInfoEvent, UpdateRestaurantInfoState> {
-  UpdateRestaurantInfoBloc() : super(UpdateRestaurantInfoBlocInitial()) {
-    on<UpdateRestaurantInfoEvent>((event, emit) {
-      // TODO: implement event handler
+  final RestaurantRepository _restaurantRepository;
+  UpdateRestaurantInfoBloc({required RestaurantRepository restaurantRepository})
+      : _restaurantRepository = restaurantRepository,
+        super(UpdateRestaurantInfoBlocInitial()) {
+    on<UploadPicture>((event, emit) async {
+      emit(UploadPictureLoading());
+      try {
+        String restaurantImage = await _restaurantRepository.uploadPicture(
+            event.file, event.restaurantId);
+        emit(UploadPictureSuccess(restaurantImage));
+      } catch (e) {
+        emit(UploadPictureFailure());
+      }
     });
   }
 }
