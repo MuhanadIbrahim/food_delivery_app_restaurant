4bb45a3b9a5212b3f5ae522878c1f08cbcdd5aea
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-12-12 20:59:44 +0200

    still worcking in main ui of the home page

--- lib/presentation/screans/home_screan.dart
+++ lib/presentation/screans/home_screan.dart
@@ -102,6 +102,141 @@ class HomeScrean extends StatelessWidget {
                 )
               ],
             ),
+          ),
+          SizedBox(
+            height: 15.h,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.r),
+            height: 80.h,
+            width: 340.w,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.r),
+              color: Colors.white,
+              border: Border.all(
+                width: 2,
+                color: Colors.black,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'Meals Request',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      '23',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '+5,1%',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_upward, color: Colors.black),
+                      onPressed: () {
+                        // Add your onPressed functionality here
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15.h,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(18.r),
+              height: 80.h,
+              width: double.infinity.w,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.r),
+                color: Colors.white,
+                border: Border.all(
+                  width: 2,
+                  color: Colors.black,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Region 1',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 10.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 5.h,
+                          ),
+                          Text(
+                            'Meals Request',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            height: 8.h,
+                          ),
+                          Text(
+                            '23',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '-5,09%',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          IconButton(
+                            icon:
+                                Icon(Icons.arrow_downward, color: Colors.black),
+                            onPressed: () {
+                              // Add your onPressed functionality here
+                            },
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
